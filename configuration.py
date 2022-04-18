67a8b8ee0c7bb49fefc6354012eb39c7e64dc786
bayy420-999 <97155103+bayy420-999@users.noreply.github.com>
2022-04-18 17:03:16 +0700

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -4,10 +4,10 @@ config = ConfigParser()
 config.read('config.ini')
 
 def configuration():
-  return config['account']['email'], config['account']['password'], config['account']['token'], config['configuration']['download_path'], config.getboolean('configuration', 'is_batch_download'), config['configuration']['batch_file']
+    return config['account']['email'], config['account']['password'], config['account']['token'], config['configuration']['download_path'], config.getboolean('configuration', 'is_batch_download'), config['configuration']['batch_file']
 
 def write_token(args):
-  for config['account']['token'] in config:
-    config.set('account', 'token', args)
-    with open('config.ini', 'w') as outfile:
-      config.write(outfile)
+    for config['account']['token'] in config:
+        config.set('account', 'token', args)
+        with open('config.ini', 'w') as outfile:
+            config.write(outfile)
